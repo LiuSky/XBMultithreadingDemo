158bf85c804d5ce246861f7d0c4e73dfe0a616c9
xiaobin liu <327847390@qq.com>
2018-12-29 15:02:53 +0800

    -added: Thread

---
+++ XBMultithreadingDemo/Classes/Thread/ThreadC.swift
@@ -0,0 +1,20 @@
+//
+//  ThreadC.swift
+//  XBMultithreadingDemo
+//
+//  Created by xiaobin liu on 2018/12/29.
+//  Copyright © 2018 Sky. All rights reserved.
+//
+
+import UIKit
+
+
+/// MARK - ThreadC
+final class ThreadC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationItem.title = "Thread演示"
+        self.view.backgroundColor = UIColor.white
+    }
+}
